b7249c9e84b4d14cd7742f6a9203b7d9d7acbddf
muhammvd <muhammadsaniharuna44@gmail.com>
2025-12-18 19:26:57 +0100

    eslint+fix

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -56,6 +56,7 @@ export function AuthProvider({ children }) {
     );
 
     return () => axios.interceptors.response.eject(interceptor);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   /** -------------------------------
