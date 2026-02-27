5dd0b6c1d32ddfd497992f3e51fdb501dd0b0ae2
muhammvd <muhammadsaniharuna44@gmail.com>
2026-02-27 17:53:31 +0100

    fixes

--- backend/check_db.js
+++ backend/check_db.js
@@ -6,7 +6,7 @@ async function checkMaterials() {
     console.table(res.rows);
     process.exit(0);
   } catch (err) {
-    console.error(err);
+    console.error(err)
     process.exit(1);
   }
 }
