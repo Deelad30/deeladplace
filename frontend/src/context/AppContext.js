b1a0fdb56924e0d8d868c043274258df6425bba7
Muhammad Sani Haruna <hammadmediaenterprise@gmail.com>
2026-04-24 18:53:36 -0700

    changes++

--- frontend/src/context/AppContext.js
+++ frontend/src/context/AppContext.js
@@ -5,7 +5,7 @@ const AppContext = createContext();
 export const useApp = () => {
   const context = useContext(AppContext);
   if (!context) {
-    throw new Error('useApp must be used within an AppProvider');
+    throw new Error('useApp must be used within an APPProvider');
   }
   return context;
 };
